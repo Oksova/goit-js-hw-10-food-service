edb5fa6d54fdc7ebe33cc6090599d149cae414f2
Mariia Oksova <oksova027@gmail.com>
2020-10-26 15:41:17 +0200

    Не работает шаблонизатор

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import menuTpl from '../src/templates/menuTpl.hbs'
+import menu from './menu.json'
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+}
+
+const ref = {
+  menuEl: document.querySelector('.js-menu'),
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const Handlebars = require('handlebars')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.handlebars$/,
+        use: ['handlebars-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  devServer: {
+    port: 4444,
+    open: true,
+    stats: 'errors-only',
+  },
+}
